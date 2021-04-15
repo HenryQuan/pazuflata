9d380b6744cf8d78db5689630553911fae1a10b7
Yiheng <development.henryquan@gmail.com>
2021-04-15 20:30:33 +1000

    updated pazusoba, added pazusoba.so and added windows
    try out windows, ignore web for now. I may need to use wasm for it.

---
+++ pazuflata/lib/ui/pages/home.dart
@@ -0,0 +1,35 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:ffi/ffi.dart';
+
+typedef pazusoba_func = Void Function(Int32, Pointer<Pointer<Int8>>);
+typedef Pazusoba = void Function(int, Pointer<Pointer<Int8>>);
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+
+    final libary = DynamicLibrary.open('pazusoba.so');
+    final Pazusoba pazusoba = libary
+        .lookup<NativeFunction<pazusoba_func>>('pazusoba')
+        .asFunction<Pazusoba>();
+    final Pointer<Pointer<Int8>> list = malloc();
+    pazusoba(0, list);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hello'),
+      ),
+    );
+  }
+}
